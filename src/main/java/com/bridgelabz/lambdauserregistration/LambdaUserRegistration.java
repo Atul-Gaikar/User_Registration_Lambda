54a4a93435f82ef9b58a1676f8bf92a80d831a11
Atul <atulgaikarag9@gmail.com>
2021-07-10 00:01:38 +0530

    Adding User Registration Lambda Last Name

--- src/main/java/com/bridgelabz/lambdauserregistration/LambdaUserRegistration.java
+++ src/main/java/com/bridgelabz/lambdauserregistration/LambdaUserRegistration.java
@@ -10,15 +10,15 @@ interface Registration {
 
 public class LambdaUserRegistration {
 	public static void main(String[] args) {
-		Registration FirstName = firstName -> Pattern.matches(NAME_PATTERN, firstName);
-		System.out.println(printResult(FirstName.validate(validFirstName())));
+		Registration LastName = lastName -> Pattern.matches(NAME_PATTERN, lastName);
+		System.out.println(printResult(LastName.validate(validLastName())));
 	}
 
 	private static final String NAME_PATTERN = ("^[A-Z]{1}[a-z]{2}$");
 	static Scanner input = new Scanner(System.in);
 
-	private static String validFirstName() {
-		System.out.println("Enter the First Name");
+	private static String validLastName() {
+		System.out.println("Enter the Last Name");
 		return input.nextLine();
 	}
 
