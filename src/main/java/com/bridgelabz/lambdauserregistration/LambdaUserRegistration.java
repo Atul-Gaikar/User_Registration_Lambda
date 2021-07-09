c587483f6ed335b5524ebc0671fc446e95f955c6
Atul <atulgaikarag9@gmail.com>
2021-07-10 00:19:24 +0530

    Adding User Registration Lambda Email Address

--- src/main/java/com/bridgelabz/lambdauserregistration/LambdaUserRegistration.java
+++ src/main/java/com/bridgelabz/lambdauserregistration/LambdaUserRegistration.java
@@ -11,22 +11,32 @@ interface Registration {
 public class LambdaUserRegistration {
 	public static void main(String[] args) {
 		Registration FirstName = firstName -> Pattern.matches(NAME_PATTERN, firstName);
-		Registration validateLastName = lastName -> Pattern.matches(NAME_PATTERN, lastName);
+		Registration LastName = lastName -> Pattern.matches(NAME_PATTERN, lastName);
+		Registration Email = email -> Pattern.matches(EMAIL_PATTERN, email);
 		System.out.println(printResult(FirstName.validate(validFirstName())));
-		System.out.println(printResult(validateLastName.validate(validLastName())));
+		System.out.println(printResult(LastName.validate(validLastName())));
+		System.out.println(printResult(Email.validate(validEmail())));
 	}
 
 	private static final String NAME_PATTERN = ("^[A-Z]{1}[a-z]{2}$");
+	private static final String EMAIL_PATTERN = ("^[a-z0-9+_.-]+@[a-zA-Z0-9.-]+$");
 	static Scanner input = new Scanner(System.in);
 
 	private static String validFirstName() {
 		System.out.println("Enter the First Name");
 		return input.nextLine();
 	}
+
 	private static String validLastName() {
 		System.out.println("Enter the Last Name");
 		return input.nextLine();
 	}
+
+	private static String validEmail() {
+		System.out.println("Enter The Email Address");
+		return input.next();
+	}
+
 	public static String printResult(Boolean result) {
 		if (Boolean.TRUE.equals(result))
 			return "Your Entry is Valid";
