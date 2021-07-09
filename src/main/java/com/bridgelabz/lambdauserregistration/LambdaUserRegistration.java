50465d1f1d32f95042816983aebf436a6286ee35
Atul <atulgaikarag9@gmail.com>
2021-07-10 00:34:15 +0530

    Adding User Registration Lambda Password Format

--- src/main/java/com/bridgelabz/lambdauserregistration/LambdaUserRegistration.java
+++ src/main/java/com/bridgelabz/lambdauserregistration/LambdaUserRegistration.java
@@ -14,15 +14,18 @@ public class LambdaUserRegistration {
 		Registration LastName = lastName -> Pattern.matches(NAME_PATTERN, lastName);
 		Registration Email = email -> Pattern.matches(EMAIL_PATTERN, email);
 		Registration MobileNumber = mobileNumber -> Pattern.matches(MOBILE_NUMBER_PATTERN, mobileNumber);
+		Registration PasswordFormat = password -> Pattern.matches(PASSWORD_PATTERN, password);
 		System.out.println(printResult(FirstName.validate(validFirstName())));
 		System.out.println(printResult(LastName.validate(validLastName())));
 		System.out.println(printResult(Email.validate(validEmail())));
 		System.out.println(printResult(MobileNumber.validate(validMobileNumber())));
+		System.out.println(printResult(PasswordFormat.validate(validPassword())));
 	}
 
 	private static final String NAME_PATTERN = ("^[A-Z]{1}[a-z]{2}$");
 	private static final String EMAIL_PATTERN = ("^[a-z0-9+_.-]+@[a-zA-Z0-9.-]+$");
 	private static final String MOBILE_NUMBER_PATTERN = ("^[7-9][0-9]{9}$");
+	private static final String PASSWORD_PATTERN = ("^[a-zA-Z0-9+_.@-]{8}+$");
 	static Scanner input = new Scanner(System.in);
 
 	private static String validFirstName() {
@@ -42,6 +45,11 @@ public class LambdaUserRegistration {
 
 	private static String validMobileNumber() {
 		System.out.println("Enter The Mobile Number");
+		return input.next();
+	}
+
+	private static String validPassword() {
+		System.out.println("Enter The PassWord Format");
 		return input.next();
 	}
 
